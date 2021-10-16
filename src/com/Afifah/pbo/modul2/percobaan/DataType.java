3baffb924b2cd436ff8ab705de3fb171beac17c5
Afifah Vollyani Saktiana <82068415+Afifah-vs@users.noreply.github.com>
2021-10-17 00:35:24 +0700

    class for data type of java

---
+++ src/com/Afifah/pbo/modul2/percobaan/DataType.java
@@ -0,0 +1,31 @@
+package com.Afifah.pbo.modul2.percobaan;
+
+public class DataType {
+
+    public static void main(String[] args) {
+
+        // Tipe data primitif
+
+        long data1 = 767226531;     // Bilangan bulat
+        int data2 = 2235641;        // Bilangan bulat
+        short data3 = 714;          // Bilangan bulat
+        byte data4 = 34;            // Bilangan bulat
+
+        double data5 = 4.967;        // Bilangan desimal
+        float data6 = (float) 1.733; // Bilangan desimal
+
+        char data7 = 'C';            // Hanya berisi
+        boolean data8 = true;
+
+        // Output
+        System.out.println("Nilai data1 = " + data1);
+        System.out.println("Nilai data2 = " + data2);
+        System.out.println("Nilai data3 = " + data3);
+        System.out.println("Nilai data4 = " + data4);
+        System.out.println("Nilai data5 = " + data5);
+        System.out.println("Nilai data6 = " + data6);
+        System.out.println("Nilai data7 = " + data7);
+        System.out.println("Nilai data8 = " + data8);
+
+    }
+}
