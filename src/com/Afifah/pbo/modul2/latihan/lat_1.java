79683111757bd7fefe4d482efc57e4b8a75bab19
Afifah Vollyani Saktiana <82068415+Afifah-vs@users.noreply.github.com>
2021-10-18 12:23:32 +0700

    Lat_1 class for long data type error analysis

---
+++ src/com/Afifah/pbo/modul2/latihan/lat_1.java
@@ -0,0 +1,14 @@
+package com.Afifah.pbo.modul2.latihan;
+
+public class lat_1 {
+
+    public static void main(String[] args) {
+        main(String args[]) {
+            long p=2147483648;
+
+
+        }
+}
+
+
+
