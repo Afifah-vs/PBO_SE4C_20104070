85e116a9bd49c4a9b3cfd81914527920a1ecc1a7
Afifah Vollyani Saktiana <82068415+Afifah-vs@users.noreply.github.com>
2021-11-01 20:50:43 +0700

    Class Tabungan berisi konstruktor

---
+++ src/com/Afifah/pbo/modul4/percobaan/Tabungan.java
@@ -0,0 +1,60 @@
+package com.Afifah.pbo.modul4.percobaan;
+
+public class Tabungan {
+    private String nama;
+    private int noRekening;
+    private  int saldo;
+    private int pin;
+
+    public Tabungan(String nama, int noRekening){
+        this.nama = nama;
+        this.noRekening = noRekening;
+        this.saldo = 0;     // Nilai default saldo
+        this.pin = 12345;   // Nilai default pin
+    }
+
+    public Tabungan(String nama, int noRekening, int saldo, int pin) {
+        this.nama = nama;
+        this.noRekening = noRekening;
+        this.saldo = saldo;
+        this.pin = pin;
+    }
+
+    public void simpanUang(int jumlah){
+        this.saldo += jumlah;
+    }
+
+    public void tarikUang(int jumlah){
+        this.saldo -=jumlah;
+    }
+
+    public void setSaldo(int saldo){
+        this.saldo = saldo;
+    }
+
+    public void setPin(int pin){
+        this.pin = pin;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getNoRekening() {
+
+        return noRekening;
+    }
+
+    public int getSaldo() {
+
+        return saldo;
+    }
+
+    public int getPin() {
+
+        return pin;
+    }
+}
+
+
+
