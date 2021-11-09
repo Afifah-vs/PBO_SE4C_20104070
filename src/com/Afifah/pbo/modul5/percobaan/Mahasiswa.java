24dc4d2a2c427ccbbcd3163824d4aa246bfe5854
Afifah Vollyani Saktiana <82068415+Afifah-vs@users.noreply.github.com>
2021-11-09 08:24:46 +0700

    Mahasiswa class

---
+++ src/com/Afifah/pbo/modul5/percobaan/Mahasiswa.java
@@ -0,0 +1,30 @@
+package com.Afifah.pbo.modul5.percobaan;
+
+public class Mahasiswa {
+
+    private int nrp;
+    private String nama;
+
+    public Mahasiswa() {
+
+        this(0, "null");
+    }
+
+    public Mahasiswa(String nama) {
+
+        this(0,nama);
+    }
+
+    public Mahasiswa(int nrp, String nama){
+        this.nrp = nrp;
+        this.nama = nama;
+    }
+
+    public int getNrp() {
+        return nrp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
