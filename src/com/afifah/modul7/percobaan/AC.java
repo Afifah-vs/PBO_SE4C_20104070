b6abd7d7c5384eb06e65badcfa67dd63d242c29d
Afifah Vollyani Saktiana <82068415+Afifah-vs@users.noreply.github.com>
2021-11-23 01:11:56 +0700

    AC class is extends Electronics class

---
+++ src/com/afifah/modul7/percobaan/AC.java
@@ -0,0 +1,20 @@
+package com.Afifah.modul7.percobaan;
+
+public class AC extends Electonics {
+
+    public AC() {
+    }
+    public AC(String name) {
+        super(name);
+    }
+
+    public void onActivate() {
+        System.out.println(this.getName() + " starts cooling the room!");
+    }
+
+    public void onDeactivate() {
+        System.out.println(this.getName() + " stops cooling the room! ");
+    }
+}
+
+
