e854b4457c7ebe9b26c0e2ee9e88563d4a63ac96
Afifah Vollyani Saktiana <82068415+Afifah-vs@users.noreply.github.com>
2021-11-23 01:12:19 +0700

    TV class is extends Electronics class

---
+++ src/com/afifah/modul7/percobaan/TV.java
@@ -0,0 +1,22 @@
+package com.Afifah.modul7.percobaan;
+
+public class TV extends Electonics{
+
+    public TV() {
+    }
+
+    public TV(String name) {
+        super(name);
+    }
+
+    public void onActivate() {
+        System.out.println(this.getName() + " starts displaying image!");
+    }
+
+    public void onDeactivate() {
+        System.out.println(this.getName() + " stops displaying game");
+    }
+}
+
+
+
